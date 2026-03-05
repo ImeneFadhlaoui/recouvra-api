fd004fe8e80b250fc2427977eba5587361f90d0e
Imene Fadhlaoui <imenefadhlaoui19@gmail.com>
2026-03-05 01:16:34 +0100

    feat: add database connection

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 
-const express = require('express')
+const express = require('express');
+const connectDB = require('./src/config/database');
 const app = express();
 
 const PORT = process.env.PORT
 
 
-app.listen(PORT, () => {
+const startServer = async () => {
+    await connectDB();
+    app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 })
+}
+
+startServer();
+
+
